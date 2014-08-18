11ae468fa1351c6599acfc178c1be687995ca464
Yauhen Artsiukhou <jsirex@gmail.com>
2014-08-18 14:36:17 +0300

    Adding default skeleton files

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+# get this from git for the chefignore issues
+gem 'berkshelf', '~> 3.0'
+gem 'chef-zero'
+
+group 'develop' do
+  gem 'test-kitchen'
+  gem 'kitchen-vagrant'
+  gem 'kitchen-docker'
+  gem 'rake'
+  gem 'foodcritic', '>= 3.0.3'
+  gem 'rubocop', '~> 0.24'
+  gem 'knife-cookbook-doc'
+  gem 'knife-spork'
+  gem 'chefspec', '~> 4.0'
+  gem 'git'
+end
+
+group 'ci' do
+  gem 'ci_reporter_rspec'
+end
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,13 @@
-name             "collectd"
-maintainer       "Noan Kantrowitz"
-maintainer_email "noah@coderanger.net"
-license          "Apache 2.0"
-description      "Install and configure the collectd monitoring daemon"
+name             'collectd'
+maintainer       'Noan Kantrowitz'
+maintainer_email 'noah@coderanger.net'
+license          'Apache 2.0'
+description      'Install and configure the collectd monitoring daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-supports         "ubuntu"
+version          '1.1.0'
+
+supports 'debian'
+supports 'ubuntu'
+
+suggests 'apache2'
+suggests 'collectd_plugins'
